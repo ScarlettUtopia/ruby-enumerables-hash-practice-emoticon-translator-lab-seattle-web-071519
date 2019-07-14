f9949dfbe594656468a73d5855ef87419153530e
Megan Scarlett <scarlettutopia@aol.com>
2019-07-14 22:36:05 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,7 @@ def load_library => {
   :angel => {"O:)","☜(⌒▽⌒)☞"},
   :angry => {">:(","ヽ(ｏ`皿′ｏ)ﾉ"},
   :bored => { ":O","(ΘεΘ;)"},
-  :confused => {},
+  :confused => {"%)","(゜.゜)"},
   :embarrased => {},
   :fish => {},
   :glasses => {},
@@ -21,6 +21,7 @@ def get_japanese_emoticon
  O:) = "☜(⌒▽⌒)☞"
   >:( = "ヽ(ｏ`皿′ｏ)ﾉ"
   :O = "(ΘεΘ;)"
+  
 end
 
 def get_english_meaning
