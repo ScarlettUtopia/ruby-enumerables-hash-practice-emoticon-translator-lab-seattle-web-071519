63c8a248c60df6f997e564686f2cccfd1c4e0f3a
Megan Scarlett <scarlettutopia@aol.com>
2019-07-14 22:30:56 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,20 @@
 # require modules here
 
-def load_library
-  # code goes here
+def load_library => {
+  :angel => {},
+  :angry => {},
+  :bored => {},
+  :confused => {},
+  :embarrased => {},
+  :fish => {},
+  :glasses => {},
+  :grinning => {},
+  :happy => {},
+  :kiss => {},
+  :sad => {},
+  :surprised => {},
+  :wink => {}
+}
 end
 
 def get_japanese_emoticon
@@ -11,3 +24,42 @@ end
 def get_english_meaning
   # code goes here
 end
+angel:
+  - "O:)"      
+  - "☜(⌒▽⌒)☞"
+angry:
+  - ">:("
+  - "ヽ(ｏ`皿′ｏ)ﾉ"
+bored:
+  - ":O"
+  - "(ΘεΘ;)"
+confused:
+  - "%)"
+  - "(゜.゜)"
+embarrased:
+  - ":$" 
+  - "(#^.^#)"
+fish:
+  - "><>"
+  - ">゜))))彡"
+glasses:
+  - "8D"
+  - "(^0_0^)"
+grinning:
+  - "=D"
+  - "(￣ー￣)"
+happy:
+  - ":)"
+  - "(＾ｖ＾)"
+kiss:
+  - ":*"
+  - "(*^3^)/~☆"
+sad:
+  - ":'("
+  - "(Ｔ▽Ｔ)"
+surprised:
+  - ":o"
+  - "o_O"
+wink:
+  - ";)"
+  - "(^_-)"
